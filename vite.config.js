bc0f225a0a420277d034ce5b031b7ab1a81d2160
sudha-sambath <sudhasambath.it@gmail.com>
2026-06-17 16:16:16 -0400

    Deploy using GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/https://github.com/sudha-sambath/e-plantShopping",
   plugins: [react()],
 })
