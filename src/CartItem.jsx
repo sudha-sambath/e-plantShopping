88626ef66490441a56fcda357e897009d4aa5c65
sudha-sambath <sudhasambath.it@gmail.com>
2026-06-17 11:34:18 -0400

    CartItems component added

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,52 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        let total = 0;
+
+        cart.forEach(item => {
+            const quantity = item.quantity;
+            const cost = parseFloat(item.cost.substring(1));
+
+            total += cost * quantity;
+        });
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(
+        updateQuantity({
+          id: item.id,
+          quantity: item.quantity + 1,
+        })
+      );
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(
+        updateQuantity({
+          id: item.id,
+          quantity: item.quantity - 1,
+        })
+      );
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1));
+    return unitPrice * item.quantity;
   };
 
   return (
